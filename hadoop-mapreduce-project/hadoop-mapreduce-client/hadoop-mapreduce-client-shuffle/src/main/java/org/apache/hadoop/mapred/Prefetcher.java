c554dbc82f6584e00010ea66379ac00f7c0cf220
Mark Mansi <markm@cs.wisc.edu>
2017-11-28 12:54:43 -0600

    Working on prefetcher implementation

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/Prefetcher.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/Prefetcher.java
@@ -15,11 +15,59 @@ public class Prefetcher {
      * Represents a single stream that this prefetcher is tracking.
      */
     private class PrefetchStream {
+        // The location and length of this stream
+        private String filename;
+        private long offset;
+        private long length;
+
+        // The number of bytes from this stream that have been prefetched so far...
+        private long prefetched;
+
+        /**
+         * Construct a new prefetch stream for the given location and length.
+         * @param filename the file to read from
+         * @param offset the offset into the file to read
+         * @param length the length of the stream
+         */
+        public PrefetchStream(String filename, long offset, long length) {
+            this.filename = filename;
+            this.offset = offset;
+            this.length = length;
+            this.prefetched = 0;
+        }
+
         /**
          * Issues a prefetch from this stream to the given buffer.
          */
         public void prefetch(PrefetchBuffer buffer) {
             // TODO
+        }
+
+        /**
+         * @return true if the given location is in this stream. Otherwise, false.
+         */
+        public boolean contains(String filename, long offset) {
+            // FIXME: need to normalize file names first to deal with e.g. relative
+            // vs absolute paths to the same file...
+            if (!filename.equals(this.filename)) {
+                return false;
+            }
+
+            return offset >= this.offset && offset < this.offset + this.length;
+        }
+
+        /**
+         * @return the offset _in the file_ of the first byte not yet prefetched.
+         */
+        public long latestPrefetched() {
+            return this.offset + this.prefetched;
+        }
+
+        /**
+         * Truncate this stream at whatever has already been prefetched.
+         */
+        public void truncate() {
+            this.length = this.prefetched;
         }
     }
 
@@ -46,7 +94,8 @@ public class Prefetcher {
     private final HashMap<String, ArrayList<PrefetchStream>> streamInfo;
 
     /**
-     * An LRU list, where the head is the LRU, and the tail is the MRU
+     * An LRU list, where the head (first) is the LRU, and the tail (last) is
+     * the MRU.
      */
     private final LinkedList<PrefetchStream> lruOrder;
 
@@ -58,9 +107,9 @@ public class Prefetcher {
      * Create a new Prefetcher...
      */
     private Prefetcher() {
-        buffer = new PrefetchBuffer();
-        streamInfo = new HashMap<>();
-        lruOrder = new LinkedList<>();
+        this.buffer = new PrefetchBuffer();
+        this.streamInfo = new HashMap<>();
+        this.lruOrder = new LinkedList<>();
     }
 
     /**
@@ -84,7 +133,7 @@ public class Prefetcher {
         // If the request is for something that was not previous prefetched
         // (whether it was never seen or only partially prefetched), make it
         // the LRU stream.
-        fetchStream(filename, offset);
+        fetchStream(filename, offset, buf.length);
 
         // Issue a prefetch for the LRU stream. Make sure it is enough to
         // satisfy this request. (FIXME: maybe we want something else here?)
@@ -95,15 +144,71 @@ public class Prefetcher {
         moveToMRU(stream);
 
         // Issue a read to the buffer to get the data for this request.
-        return buffer.read(filename, offset, buf);
+        return this.buffer.read(filename, offset, buf);
     }
 
     /** If the request is for something that was not previous prefetched
      * (whether it was never seen or only partially prefetched), make it the
      * LRU stream. Otherwise, do nothing.
      */
-    private void fetchStream(String filename, long offset) {
-        // TODO
+    private void fetchStream(String filename, long offset, long length) {
+        // Get the list of streams for this file
+        ArrayList<PrefetchStream> fileStreams =
+            this.streamInfo.get(filename);
+
+        if (fileStreams == null) {
+            fileStreams = new ArrayList<>();
+            this.streamInfo.put(filename, fileStreams);
+        }
+
+        // Look for one with the the given offset
+        PrefetchStream match = null;
+        for (PrefetchStream ps : fileStreams) {
+            if (ps.contains(filename, offset)) {
+                match = ps;
+                break;
+            }
+        }
+
+        // If one is found, check what the most recently prefetched offset in
+        // that stream is.
+        if (match != null && match.latestPrefetched() >= offset + length) {
+            // If the desired portion was prefetched, then we are done
+            return;
+        }
+
+        // If we got here, the there is some portion of the request
+        // that was not already prefetched. Create a new stream for that
+        // portion and make it the LRU.
+        long notYetStart;
+        long notYetLength;
+
+        if (match != null) {
+            // There was a stream that was prefetching at least part of the
+            // requested portion of the file.
+            long end = offset + length;
+            notYetStart = match.latestPrefetched();
+            notYetLength = end - notYetStart;
+
+            // We are about to create a new stream for the rest of the old
+            // stream, so end the old stream here. This simplifies the logic
+            // of prefetching because we can always focus on prefetching just
+            // a single stream.
+            match.truncate();
+        } else {
+            // No matching existing stream was found, so create one.
+            notYetStart = offset;
+            notYetLength = length;
+        }
+
+        // Create a new stream
+        PrefetchStream newStream = new PrefetchStream(filename,
+                                                      notYetStart,
+                                                      notYetLength);
+
+        // Make it the LRU
+        this.streamInfo.get(filename).add(newStream);
+        this.lruOrder.addFirst(newStream);
     }
 
     /**
@@ -112,8 +217,7 @@ public class Prefetcher {
      * NOTE: the prefetcher's table is assumed to be non-empty!
      */
     private PrefetchStream getLRUStream() {
-        // TODO
-        return new PrefetchStream();
+        return this.lruOrder.getFirst();
     }
 
     /**
@@ -124,10 +228,10 @@ public class Prefetcher {
      */
     private void moveToMRU(PrefetchStream stream) {
         // Remove from the LRU list
-        boolean exists = lruOrder.remove(stream);
+        boolean exists = this.lruOrder.remove(stream);
         assert(exists);
 
         // Append to the tail (MRU)
-        lruOrder.add(stream);
+        this.lruOrder.addLast(stream);
     }
 }
