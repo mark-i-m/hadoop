8b8279884afe5d20a0453e61b11bcbda0a301103
Mark Mansi <markm@cs.wisc.edu>
2017-12-11 13:48:30 -0600

    Add logging, fix incorrect ordering for RandomAccessPolicy

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/RandomAccessPolicy.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/RandomAccessPolicy.java
@@ -6,6 +6,9 @@ import java.util.LinkedList;
 import org.apache.hadoop.mapred.PrefetchPolicy;
 import org.apache.hadoop.mapred.PrefetchPolicy.Prefetch;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * A policy that is optimized for lots of reducers making small reads.
  */
@@ -14,6 +17,11 @@ public class RandomAccessPolicy implements PrefetchPolicy {
      * A queue of Prefetches to be made
      */
     private LinkedList<Prefetch> queue = new LinkedList<>();
+
+    /**
+     * Log object for logging...
+     */
+    private static final Log LOG = LogFactory.getLog(RandomAccessPolicy.class);
 
     /**
      * If we have enough memory, then just use the oldest waiting prefetch.
@@ -27,6 +35,9 @@ public class RandomAccessPolicy implements PrefetchPolicy {
                          int reducerId)
     {
         // TODO: maybe we want to break up large requests?
+        LOG.info("Request: file=" + filename + ", off=" + offset + ", len=" + length
+                  + ", memUsage=" + memUsage + ", memBound=" + memBound
+                  + ", reducerId=" + reducerId);
 
         // Enqueue the new request
         Prefetch prefetch = new Prefetch(filename, offset, length);
@@ -35,10 +46,18 @@ public class RandomAccessPolicy implements PrefetchPolicy {
         // If the oldest request does not take up too much space, dequeue it
         // and return it.
         Prefetch oldest = queue.peekFirst();
-        if (oldest != null && oldest.length < (memBound - memUsage)) {
-            return queue.pollLast();
-        } else {
+
+        if (oldest == null) {
+            LOG.info("Nothing to prefetch");
             return null;
         }
+
+        if (oldest.length > (memBound - memUsage)) {
+            LOG.info("Oldest is too large: " + oldest);
+            return null;
+        }
+
+        LOG.info("Policy chose: " + oldest);
+        return queue.pollFirst();
     }
 }
