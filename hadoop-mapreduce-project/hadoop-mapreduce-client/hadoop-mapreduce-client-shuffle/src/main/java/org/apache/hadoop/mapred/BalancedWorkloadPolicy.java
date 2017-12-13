6ec1c6a58f4c708fb94df15d9792e8ef552c8ff6
Shiqi Yang <shiqi.yang1994@gmail.com>
2017-12-12 20:40:13 -0600

    improved balanced workload

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/BalancedWorkloadPolicy.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-shuffle/src/main/java/org/apache/hadoop/mapred/BalancedWorkloadPolicy.java
@@ -6,6 +6,9 @@ import org.apache.hadoop.mapred.PrefetchPolicy.Prefetch;
 import java.io.File;
 import java.util.HashMap;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * A policy based on the assumption that the workload is balanced.
  */
@@ -14,7 +17,9 @@ public class BalancedWorkloadPolicy implements PrefetchPolicy {
     // value is true if key has been prefetched
     protected HashMap<Integer, Boolean> prefetched = new HashMap<>(); 
     // TODO: how to obtain?
-    int numReduceTasks = 5;     // for now...
+    int numReduceTasks = 1;     // for now...
+
+    private static final Log LOG = LogFactory.getLog(BalancedWorkloadPolicy.class);
 
     public Prefetch next(String filename,
                          long offset,
@@ -23,22 +28,24 @@ public class BalancedWorkloadPolicy implements PrefetchPolicy {
                          long memBound,
                          int reducerId)
     {
-        long fileSize = (new File(filename)).length();
-        String strNumReduceTasks = System.getenv("NUM_REDUCE");
-        if (strNumReduceTasks != null)
-            numReduceTasks = Integer.parseInt(strNumReduceTasks);
         if (!prefetched.containsKey(reducerId)) {
         	prefetched.put(reducerId, false);
         }
         
         if (prefetched.get(reducerId)) {
+            LOG.info("Nothing to prefetch");
         	// assume it has been prefetched
         	return null;
         } else {
         	// issue a new prefetch request
+            long fileSize = (new File(filename)).length();
+            String strNumReduceTasks = System.getenv("NUM_REDUCE");
+            if (strNumReduceTasks != null)
+                numReduceTasks = Integer.parseInt(strNumReduceTasks);
         	long prefetchLength = fileSize / numReduceTasks;
             long startOffset = reducerId * prefetchLength;
-        	if (prefetchLength >= memBound - memUsage) {
+        	
+            if (prefetchLength >= memBound - memUsage) {
                 startOffset = offset;
         		prefetchLength = length;
         	}
@@ -46,6 +53,8 @@ public class BalancedWorkloadPolicy implements PrefetchPolicy {
             if (prefetchLength >= memBound - memUsage) return null;
             
             prefetched.put(reducerId, true);
+            LOG.info("Issue a new prefetch request: " + filename + 
+                " offset=" + startOffset + " length=" + prefetchLength);
         	return new Prefetch(filename, startOffset, prefetchLength);
         }
     }
